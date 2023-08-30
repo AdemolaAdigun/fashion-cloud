52f977d4ca42a9e581a380d0c5f7577e141e414d
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-08-30 18:28:23 +0200

    Refactored and added routes to app entry point

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,32 +1,38 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { connectToDatabase } from './db-connector';
 
-import DBConnector from './db-connector';
 import Config from './config';
+import routes from "./route/index";
 
 const app = express();
 
-const connectDatabases = async () => {
-    await DBConnector.connectMongo(Config.MONGO_URL + Config.FASHION_CLOUD_DB);
-};
-
-const addBodyParser = async () => {
-    app.use(bodyParser.urlencoded({extended: false}));
-    app.use(bodyParser.json());
-};
-
-const listenPort = (PORT) => {
-    app.listen( PORT, () =>
-        console.log(`Server running on http://localhost:${PORT}`)
-    );
-};
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes)
 
 async function start() {
-    await connectDatabases();
-    await addBodyParser();
-    await listenPort(Config.SERVICE_PORT);
-};
+    const { MONGO_URL, FASHION_CLOUD_DB, SERVICE_PORT } = Config;
+
+    // Connect to the database
+    try {
+        await connectToDatabase(`${MONGO_URL}${FASHION_CLOUD_DB}`);
+    } catch (err) {
+        console.error("Failed to connect to the database", err);
+        process.exit(1);  // Exit the process with an error code
+    }
+
+    app.get('/', (req, res) => {
+        res.send('Welcome to Fashion Cloud!');
+    });
+
+    // Start the server
+    app.listen(SERVICE_PORT, () => {
+        console.log(`Server running on http://localhost:${SERVICE_PORT}`);
+    });
+}
 
 export default {
     start
-}
+};
