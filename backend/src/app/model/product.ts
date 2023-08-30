2413871381f486c5b1235e429b88aeba9904b67d
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-08-30 18:22:39 +0200

    Updated product model and cleaned code

--- backend/src/app/model/product.ts
+++ backend/src/app/model/product.ts
@@ -1,18 +1,30 @@
-import * as mongoose from 'mongoose';
+import mongoose, { Document, Schema, Model } from 'mongoose';
 
-const ProductSchema = new mongoose.Schema(
+export interface IProduct extends Document {
+        gtin: number;
+        name: string;
+        image: string;
+        brand: string;
+        category: string;
+        color: string;
+        stock: number;
+        price: number;
+}
+
+const ProductSchema: Schema = new Schema(
     {
-        gtin: { type: Number, required: true },
-        name: { type: String, required: true },
-        image: { type: String },
-        brand: { type: String, required: true },
-        category: { type: String, required: true },
-        color: { type: String, required: true },
-        stock: { type: Number, required: true },
-        price: { type: Number, required: true }
+            gtin: { type: Number, unique: true, required: true },
+            name: { type: String, required: true },
+            image: { type: String, validate: { validator: value => /https?:\/\/.+/i.test(value), message: 'Invalid URL' } },
+            brand: { type: String, required: true },
+            category: { type: String, required: true },
+            color: { type: String, required: true },
+            stock: { type: Number, required: true, min: 0 }, // Assuming stock cannot be negative
+            price: { type: Number, required: true, min: 0 }  // Assuming price cannot be negative
     },
     { timestamps: true }
 );
 
+const Product: Model<IProduct> = mongoose.model<IProduct>('products', ProductSchema);
 
-export default mongoose.model('products', ProductSchema);
+export default Product;
