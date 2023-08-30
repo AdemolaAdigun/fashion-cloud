8bad3a184c918db4fff04981350d6307e1e40ae1
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-08-30 18:31:15 +0200

    Handled promise within index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,11 @@
 import App from './app';
 
-App.start();
+// This is the main entry point for the application.
+App.start()
+    .then(() => {
+        console.log(`App started successfully. Running in ${process.env.NODE_ENV} mode.`);
+    })
+    .catch(error => {
+        console.error("Error starting the application:", error);
+        process.exit(1); // Exit.
+    });
