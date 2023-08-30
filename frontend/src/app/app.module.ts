5d507a058310aaaeb0b0a57c0866dc62f148e74b
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-08-31 00:39:44 +0200

    Added product list component to app.module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
