a5dc814ee3f8fac4d57a06482dba77d21c268ee9
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-08-30 18:21:38 +0200

    Added dotenv package and updated env config

--- backend/src/app/config/index.ts
+++ backend/src/app/config/index.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 const SERVICE_PORT: number = parseInt(process.env.SERVICE_PORT) || 8000;
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/';
 const FASHION_CLOUD_DB = process.env.FASHION_CLOUD_DB || 'fashioncloud-assignment';
@@ -5,5 +9,5 @@ const FASHION_CLOUD_DB = process.env.FASHION_CLOUD_DB || 'fashioncloud-assignmen
 export default {
     SERVICE_PORT,
     MONGO_URL,
-    FASHION_CLOUD_DB
+    FASHION_CLOUD_DB,
 }
