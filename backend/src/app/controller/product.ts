2724d9afb51ce9ea54651dd488bf93424a5398f6
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-08-30 18:25:11 +0200

    Added product controller methods and routes

---
+++ backend/src/app/controller/product.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+export default {
+    async getProducts(req: Request, res: Response): Promise<void> {
+    },
+
+    async createProduct(req: Request, res: Response): Promise<void> {
+    },
+
+    async updateProductById(req: Request, res: Response): Promise<void> {
+    },
+
+    async deleteProductById(req: Request, res: Response): Promise<void> {
+    }
+};
---
+++ backend/src/app/route/product.ts
@@ -0,0 +1,12 @@
+import {Router} from "express";
+import routesController from "../controller/product";
+const routes = Router();
+
+const { getProducts, createProduct, updateProductById, deleteProductById } = routesController;
+
+routes.get('/', getProducts);
+routes.post('/', createProduct);
+routes.patch('/:id', updateProductById);
+routes.delete('/:id', deleteProductById);
+
+export default routes;
