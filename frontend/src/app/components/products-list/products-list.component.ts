fe2a87b4a2c76bfc5bdec58110b6b00ebdfd8041
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-09-01 00:15:30 +0200

    Added code comments and used variable names for less and actual image directory

--- frontend/src/app/components/products-list/products-list.component.ts
+++ frontend/src/app/components/products-list/products-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from "../../services/product.service";
-import {FilterOptionsService} from "../../services/filter-options.service";
-import {ProductResponse} from "../../interfaces/product-response";
+import { ProductService } from "../../services/product.service";
+import { FilterOptionsService } from "../../services/filter-options.service";
+import { ProductResponse } from "../../interfaces/product-response";
 
 @Component({
   selector: 'app-products-list',
@@ -10,15 +10,25 @@ import {ProductResponse} from "../../interfaces/product-response";
 })
 export class ProductsListComponent implements OnInit {
 
-  public products: ProductResponse
+  // Holds the current products and their count.
+  public products: ProductResponse;
 
+  /**
+   * @param productService - Service to fetch products.
+   * @param filterOptionsService - Service to obtain the current filter options.
+   */
   constructor(private productService: ProductService, private filterOptionsService: FilterOptionsService) {
+    // Initialize with an empty products array and count of zero.
     this.products = {
       products: [],
       count: 0
     };
   }
 
+  /**
+   * On component initialization, subscribe to filter options changes.
+   * When a new filter option is provided, fetch products based on those options.
+   */
   async ngOnInit(): Promise<void> {
     this.filterOptionsService.filterOptions$.subscribe(options => {
       this.productService.getProducts(options).then((response: ProductResponse) => {
@@ -27,5 +37,4 @@ export class ProductsListComponent implements OnInit {
       });
     });
   }
-
 }
