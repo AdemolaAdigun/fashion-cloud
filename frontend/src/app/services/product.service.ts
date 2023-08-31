fdbc512f24a0d3030c88a4ffc8d230e5dcf8e524
Kabir Ademola Adigun <KabirAdemola.Adigun@planonsoftware.com>
2023-09-01 00:15:59 +0200

    Added code comments

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {lastValueFrom} from "rxjs";
-import {environment} from "../../environments/environment";
-import {Product} from "../interfaces/product";
-import {FilterOptions} from "../interfaces/filter-options";
-import {ProductResponse} from "../interfaces/product-response";
+import { HttpClient } from "@angular/common/http";
+import { lastValueFrom } from "rxjs";
+import { environment } from "../../environments/environment";
+import { Product } from "../interfaces/product";
+import { FilterOptions } from "../interfaces/filter-options";
+import { ProductResponse } from "../interfaces/product-response";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+
   constructor(private http: HttpClient) { }
 
+  /**
+   * Fetch products based on the given filter options.
+   * @param filterOptions Filter conditions.
+   * @returns A promise containing a product response object.
+   */
   public async getProducts(filterOptions: FilterOptions | null): Promise<ProductResponse> {
     try {
-
       let params: { [key: string]: string } = {};
 
       if (filterOptions) {
@@ -22,7 +27,7 @@ export class ProductService {
       }
 
       const response = await lastValueFrom(
-        this.http.get<Product[]>(`${environment.API_URL}products`, { params  })
+        this.http.get<Product[]>(`${environment.API_URL}products`, { params })
       );
       return { products: response, count: response.length };
 
@@ -32,6 +37,11 @@ export class ProductService {
     }
   }
 
+  /**
+   * Fetch a list of filter options based on the given endpoint.
+   * @param endpoint The endpoint for the filter option (e.g. 'brands', 'categories').
+   * @returns A promise containing an array of filter options.
+   */
   public async getFilterArray(endpoint: string): Promise<string[]> {
     try {
       const response = await lastValueFrom(this.http.get<string[]>(`${environment.API_URL}products/${endpoint}`));
@@ -42,6 +52,11 @@ export class ProductService {
     }
   }
 
+  /**
+   * Build URL parameters from the given filter options.
+   * @param options The filter options.
+   * @returns A map of query parameters.
+   */
   private buildParams(options: FilterOptions): { [key: string]: string } {
     return Object.entries(options)
       .filter(([_, value]) => value !== undefined)
